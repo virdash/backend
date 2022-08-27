b2b1dc938333b7adc946a583c8cc150bf2817257
igori <igorisolomon@gmail.com>
2022-08-27 14:25:27 +0100

    Updated URL

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/tweets', include('tweets.urls')),
+    path('v1/tweets', include('tweets.urls')),
 ]
